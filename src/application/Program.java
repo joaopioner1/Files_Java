5cff7e138e669bba786df71a47545c2f9c691dd7
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-05-20 08:45:23 -0300

    showing paths and creating folders

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,43 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String[] lines = new String[] {"Good morning", "Good evening", "Good night"};
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter a folder path: ");
+		String strPath = in.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);//create a file array that contains each folder's path
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+		System.out.println("");
+		
+		File[] files = path.listFiles(File::isFile);//create a file array that contains each file's path
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\newFolder").mkdir();
+		System.out.println("Directory created successfully " + success);
+		in.close();
+	}
+}
+/*
+ String[] lines = new String[] {"Good morning", "Good afternoon", "Good evening", "Good night"};
 		
 		String path = "D:\\out.txt";
 		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
 			for (String line : lines) {
 				bw.write(line);
 				bw.newLine();
@@ -21,5 +46,4 @@ public class Program {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-}
+ */
