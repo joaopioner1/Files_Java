645900170fc0612c94c5903840926d55a07912d1
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-05-20 08:14:14 -0300

    creating a new archieve using bw and fw

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,25 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String path = "D:\\jesus.txt";
+		String[] lines = new String[] {"Good morning", "Good evening", "Good night"};
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String line = br.readLine();
-			
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		String path = "D:\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
 		}
 		catch (IOException e) {
-			System.out.println("ERROR: " + e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
